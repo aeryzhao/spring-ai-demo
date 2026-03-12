165b66776e36329a86fe03609078aca3022331c2
aeryzhao <iszhaoxg@outlook.com>
2026-03-12 11:47:50 +0800

    🎉 初始化项目

---
+++ README.md
@@ -0,0 +1,23 @@
+# Spring AI
+
+关于 Spring AI 的使用练习。
+
+## Todo List
+
+- [ ] Chat
+- [ ] 提示词
+- [ ] 结构化输出
+- [ ] 聊天记忆入门
+- [ ] 聊天记忆自定义
+- [ ] 工具调用
+- [ ] MCP Client
+- [ ] MCP Server
+- [ ] RAG
+
+## 运行
+
+[Swagger 接口调试地址](http://localhost:8096/swagger-ui.html)
+
+## 参考文档
+- [Spring AI](https://spring.io/projects/spring-ai)
+- [Spring AI CSDN 专栏](https://blog.csdn.net/linwu_2006_2006/category_12993051.html)
---
+++ src/main/java/org/aeryzhao/springaidemo/chat/ChatClientController.java
@@ -0,0 +1,42 @@
+package org.aeryzhao.springaidemo.chat;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhaoxg
+ * @date 2026/1/19 17:01
+ */
+@RequestMapping("/ai/chat-client")
+@RestController
+public class ChatClientController {
+    private final ChatClient chatClient;
+
+    public ChatClientController(OpenAiChatModel chatModel) {
+        this.chatClient = ChatClient.create(chatModel);
+    }
+
+    @Operation(summary = "生成内容")
+    @GetMapping("/content")
+    String content(@RequestParam(value = "userInput", defaultValue = "讲个笑话") String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
+    }
+
+    @Operation(summary = "生成内容，元数据返回")
+    @GetMapping("/chatResponse")
+    ChatResponse chatResponse(@RequestParam(value = "userInput", defaultValue = "讲个笑话") String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .chatResponse();
+    }
+}
