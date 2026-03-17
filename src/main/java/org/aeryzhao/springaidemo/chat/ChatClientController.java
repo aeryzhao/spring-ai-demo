c95948f6d00d93ccf3aa9aba4d2eeadeb81f0b3d
aeryzhao <iszhaoxg@outlook.com>
2026-03-17 16:37:19 +0800

    feat: add chat client streaming example

--- src/main/java/org/aeryzhao/springaidemo/chat/ChatClientController.java
+++ src/main/java/org/aeryzhao/springaidemo/chat/ChatClientController.java
@@ -2,12 +2,14 @@ package org.aeryzhao.springaidemo.chat;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 /**
  * @author zhaoxg
@@ -19,7 +21,9 @@ public class ChatClientController {
     private final ChatClient chatClient;
 
     public ChatClientController(OpenAiChatModel chatModel) {
-        this.chatClient = ChatClient.create(chatModel);
+        this.chatClient = ChatClient.builder(chatModel)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
     }
 
     @Operation(summary = "生成内容")
@@ -38,5 +42,14 @@ public class ChatClientController {
                 .user(userInput)
                 .call()
                 .chatResponse();
+    }
+
+    @Operation(summary = "生成内容流式返回")
+    @GetMapping("/stream")
+    Flux<String> stream(@RequestParam(value = "userInput", defaultValue = "讲个笑话") String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .stream()
+                .content();
     }
 }
